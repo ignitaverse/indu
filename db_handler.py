2480f65a18e83857f9a0a16c550dbab2abe25679
ignitaverse <ignitaverse@gmail.com>
2025-11-21 17:12:51 +0530

    Update db_handler.py

--- db_handler.py
+++ db_handler.py
@@ -1,30 +1,26 @@
+# db_handler.py (Updated Code)
 import os
 from pymongo import MongoClient
 from datetime import datetime
 from config import Config
 
+# DBHandler अब global DB connection नहीं बनाएगा
 class DBHandler:
     def __init__(self):
-        self.client = MongoClient(Config.MONGO_URI)
-        self.db = self.client["MovieBotDB"]
-        self.users = self.db["users"]
+        # Initial client object को None पर सेट करें
+        self.client = None
+        self.db = None
+        self.users = None
 
-    def add_new_user(self, user_id: int, username: str, first_name: str):
-        if not self.users.find_one({"_id": user_id}):
-            data = {
-                "_id": user_id,
-                "username": username,
-                "first_name": first_name,
-                "first_joined": datetime.utcnow(),
-                "is_admin": False,
-            }
-            self.users.insert_one(data)
-            return True
-        return False
+    def connect(self):
+        """Initializes the MongoDB connection using Config.MONGO_URI."""
+        if not self.client:
+            # केवल तभी कनेक्ट करें जब Config validation हो चुकी हो
+            self.client = MongoClient(Config.MONGO_URI)
+            self.db = self.client["MovieBotDB"]
+            self.users = self.db["users"]
+            
+    # बाकी सारे functions (add_new_user, set_admin, is_admin) ज्यों के त्यों रहेंगे
 
-    def set_admin(self, user_id: int, value: bool = True):
-        self.users.update_one({"_id": user_id}, {"$set": {"is_admin": value}}, upsert=True)
-
-    def is_admin(self, user_id: int) -> bool:
-        user = self.users.find_one({"_id": user_id})
-        return bool(user and user.get("is_admin"))
+# DBHandler का global instance (पर अभी कनेक्ट नहीं हुआ है)
+db = DBHandler()
