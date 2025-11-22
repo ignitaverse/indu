cc11192f1952414de46a7f7bdb5687f04137be7c
ignitaverse <ignitaverse@gmail.com>
2025-11-22 14:31:27 +0530

    Update db_handler.py

--- db_handler.py
+++ db_handler.py
@@ -1,13 +1,12 @@
-# db_handler.py (Updated Code)
 import os
 from pymongo import MongoClient
 from datetime import datetime
 from config import Config
 
-# DBHandler अब global DB connection नहीं बनाएगा
+# DBHandler क्लास अब __init__ में Mongo Client को कॉल नहीं करेगी
 class DBHandler:
     def __init__(self):
-        # Initial client object को None पर सेट करें
+        # कनेक्शन को खाली रखें
         self.client = None
         self.db = None
         self.users = None
@@ -15,12 +14,12 @@ class DBHandler:
     def connect(self):
         """Initializes the MongoDB connection using Config.MONGO_URI."""
         if not self.client:
-            # केवल तभी कनेक्ट करें जब Config validation हो चुकी हो
+            # केवल तभी कनेक्ट करें जब main.py इसे कॉल करे
             self.client = MongoClient(Config.MONGO_URI)
             self.db = self.client["MovieBotDB"]
             self.users = self.db["users"]
-            
-    # बाकी सारे functions (add_new_user, set_admin, is_admin) ज्यों के त्यों रहेंगे
+    
+    # बाकी सारे functions (add_new_user, set_admin, is_admin) ज्यों के त्यों रहेंगे, 
+    # पर यह सुनिश्चित करें कि वे self.users का उपयोग करने से पहले self.client मौजूद हो।
 
-# DBHandler का global instance (पर अभी कनेक्ट नहीं हुआ है)
-db = DBHandler()
+# db = DBHandler() <--- इस लाइन को हटाएं!
