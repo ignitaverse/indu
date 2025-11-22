d1cb24f2dbe4fdea8a540e15580c383a3ac57f40
ignitaverse <ignitaverse@gmail.com>
2025-11-22 17:17:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,13 @@ import os
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-MONGO_URI = os.getenv("MONGO_URI")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
-LOG_CHAT_ID = int(os.getenv("LOG_CHAT_ID", "0"))
+class Config:
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    MONGO_URI = os.getenv("MONGO_URI")
+    ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+    LOG_CHAT_ID = int(os.getenv("LOG_CHAT_ID", "0"))
+
+    @staticmethod
+    def validate():
+        assert Config.BOT_TOKEN, "BOT_TOKEN must be set in .env file"
+        assert Config.MONGO_URI, "MONGO_URI must be set in .env file"
