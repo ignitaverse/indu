0cd9de7d0f0076331aad9331cf36f863b1320dcf
ignitaverse <ignitaverse@gmail.com>
2025-11-22 14:59:21 +0530

    Update admin_handler.py

--- handlers/admin_handler.py
+++ handlers/admin_handler.py
@@ -2,8 +2,10 @@ from pyrogram import filters
 from pyrogram.types import Message
 import config
 
-async def admin_command(client, message: Message):
+import config
+
+async def admin_command(client, message):
     if message.from_user.id == config.ADMIN_ID:
-        await message.reply("Admin command executed.")
-    else:
+        await message.reply("You are admin!")
+        else:
         await message.reply("You are not authorized to use this command.")
